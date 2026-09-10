e556216ea0eb8f0e41ee018029072b6196a10c97
hafidh-099 <hafinhoramsey@gmail.com>
2026-09-10 17:22:00 +0300

    add document

--- Backend/QueueXpressMobile/src/api/client.js
+++ Backend/QueueXpressMobile/src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Change this to your computer's IP address
 // On Mac/Linux: run 'ifconfig' or 'ip a' and Replace 192.168.1.5 with your actual IP
 
-const API_URL = 'http://10.4.11.80:8000/api';
+const API_URL = 'http://10.4.11.134:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
--- Backend/queuexpress/queuexpress/settings.py
+++ Backend/queuexpress/queuexpress/settings.py
@@ -143,8 +143,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
